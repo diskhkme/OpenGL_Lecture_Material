14f6bde5a1ebba7f800f33164613ea04b7e8e63d
Hyungki Kim <diskhkme@gmail.com>
2020-08-08 22:16:22 +0900

    GLFW Setup, First Triangle

---
+++ OpenGL/OpenGL/src/Application.cpp
@@ -0,0 +1,48 @@
+//https://www.glfw.org/documentation.html
+//사이트에서 GLFW 다운로드. 필요 라이브러리 dependencies에 복사 후 설정
+//아래 예제 코드실행 확인 
+
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 삼각형 그리는 Legacy 코드 추가
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f( 0.0f,  0.5f);
+		glVertex2f( 0.5f, -0.5f);
+		glEnd();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
