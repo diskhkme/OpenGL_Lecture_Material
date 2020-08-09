e2067053b923550b64b9f69a896fe6eaa5f13edf
Hyungki Kim <diskhkme@gmail.com>
2020-08-09 20:12:07 +0900

    Shader Parsing

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -1,37 +1,56 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
-// 이번에는 Vertex Array와 Shader를 이용하여 삼각형을 그리는 Modern OpenGL 방식으로 구현할 것임
-// Vertex Array는 GPU의 VRAM에 Buffer에 저장되는 데이터를 넘기는 방식을 이야기함
-// 데이터를 넘기고 나서 삼각형을 실제로 그리는 명령을 호출하는 것을 Draw call이라고 함
-// 삼각형이 어떻게 그려질지 사용자가 구현한 프로그램이 Shader임
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragSource;
+};
 
-// OpenGL은 State Machine과 비슷함. 일반적인 객체 지향 프로그램의 설계와는 다르게,
-// 현재 처리할 데이터를 선택한 후, 현재 실행해야 할 작업을 함수 호출을 통해 처리하는 방식임
-// 간단하게 두 삼각형을 화면에 그리는 의사코드로 설명하면,
+//셰이더 파일 파싱 함수
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
 
-// ---객체지향 방식
-// Triangle t1, t2; //삼각형 두 개를 정의
-// Draw(t1); //t1 객체를 전달함으로써 삼각형 1을 그림
-// Draw(t2); //t2 객체를 전달함으로써 삼각형 2를 그림
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+	
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos) //vertex 셰이더 섹션
+			{
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos) //fragment 셰이더 섹션
+			{
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			ss[(int)type] << line << '\n'; //코드를 stringstream에 삽입
+		}
+	}
 
-// ---State Machine 방식
-// Triangle t1, t2; //삼각형 두 개를 정의
-// Activate(t1); //삼각형 1을 처리중인 상태로 설정
-// Draw(); //현재 처리중인 데이터(=삼각형 1)를 화면에 그림
-// Activate(t2); //삼각형 2를 처리중인 상태로 설정
-// Draw(); //현재 처리중인 데이터(=삼각형 2)를 화면에 그림
+	return { ss[0].str(), ss[1].str() };
+}
 
-//--------Shader 컴파일 함수----------//
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
 	unsigned int id = glCreateShader(type); //셰이더 객체 생성(마찬가지)
 	const char* src = source.c_str();
-	glShaderSource(id, // 셰이더의 소스 코드 명시, 소스 코드를 명시할 셰이더 객체 id
-					1, // 몇 개의 소스 코드를 명시할 것인지
-					&src, // 실제 소스 코드가 들어있는 문자열의 주소값
-					nullptr); // 해당 문자열 전체를 사용할 경우 nullptr입력, 아니라면 길이 명시
+	glShaderSource(id, 1, &src, nullptr); // 셰이더의 소스 코드 명시
 	glCompileShader(id); // id에 해당하는 셰이더 컴파일
 	
 	// Error Handling(없으면 셰이더 프로그래밍할때 괴롭다...)
@@ -119,27 +138,14 @@ int main(void)
 	glVertexAttribPointer(0, 2,	GL_FLOAT, GL_FALSE, sizeof(float)*2, 0); //2. 데이터 해석 방법을 전달.
 
 	//---------Shader 생성---------------//
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;" //여기 있는 location = 0가, 118, 119 line의 0을 의미함
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n" //119에서 보다시피, 2개의 값만 전달했지만, 알아서 vec4로 변환해줌
-		"}\n";
+	//shader는 res/shaders/basic.shader로 소스코드를 옮김
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader"); //셰이더 코드 파싱
+	//std::cout << "Vertex : \n";
+	//std::cout << source.VertexSource << std::endl;
+	//std::cout << "Fragment : \n";
+	//std::cout << source.FragSource << std::endl;
 
-	std::string fragShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;" //출력 color
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 1.0 ,0.0, 1.0);\n" //빨간색 반환
-		"}\n";
-
-	unsigned int shader = CreateShader(vertexShader, fragShader);
+	unsigned int shader = CreateShader(source.VertexSource, source.FragSource);
 	glUseProgram(shader); //BindBuffer와 마찬가지로, 현재 셰이더 프로그램을 "작업 상태"로 놓음
 						  //draw call은 작업 상태인 셰이더 프로그램을 사용하여 작업 상태인 버퍼 데이터를 그림
 
